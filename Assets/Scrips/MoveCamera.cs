887d664487632db76a80a42cec50966a728e880a
Radeah <113103602+Radeah@users.noreply.github.com>
2023-07-10 15:25:08 +0100

    did stuff

--- Assets/Scrips/MoveCamera.cs
+++ Assets/Scrips/MoveCamera.cs
@@ -2,13 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class PlayerCam : MonoBehaviour
 {
-    public Transform player;
+    public float sensX;
+    public float sensY;
 
-    void Update()
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+    void Start()
     {
-        transform.position = player.transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
 }
 
